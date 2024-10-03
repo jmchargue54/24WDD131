dc210388fecc0ece6e1b5de61ebdb36bc884a387
jmchargue54 <jmchargue54@gmail.com>
2024-10-02 20:25:32 -0600

    blog html and css

---
+++ sandbox/unit3/arrays.js
@@ -0,0 +1,11 @@
+//  arrays.js
+const steps = ["one", "two", "three"];
+function listTemplate(step) {
+    return `<li>${step}</li>`; //the html string made from step
+};
+const stepsHtml = steps.map(listTemplate);// use map to convert the list from strings to HTML
+document.querySelector("#myList").innerHTML = stepsHtml.join(""); // set the innerHTML
+
+const grades = ['A','B','A'];
+
+
